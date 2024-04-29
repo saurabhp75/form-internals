38c8c2778c034ed0e9efa06651c9a28223fd07c0
Saurabh Prakash <saurabhp75@gmail.com>
2024-04-29 11:14:38 +0530

    Customize where we want to show error message.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Form validation
 
-![form](./form-validation.png)
+![](./form-validation.png)
 
 ## Browser mechanism for form validation
 
@@ -11,4 +11,52 @@
 ## Customization the form validation (Conform)
 
 - First we disable the default form validation so that we can add custom logic.
--
+- Then we customize the error messages.
+
+## Error interface
+
+- `validationMessage`: Property to get the validation message.
+- `setCustomValidity()`: Method to set the validation message.
+
+```javascript
+const [input] = document.getElementsByTagName('input');
+​
+// Check message displayed
+console.log(input.validationMessage);
+​
+// Override message
+input.setCustomValidity('This is invalid');
+​
+// See updated message
+console.log(input.validationMessage);
+```
+
+- `validity`: Property representing the valid state of the input.
+
+```javascript
+{
+  "badInput": false,
+  "customError": false,
+  "patternMismatch": false, // pattern
+  "rangeOverflow": false, // max
+  "rangeUnderflow": false, // min
+  "stepMismatch": false, // step
+  "tooLong": false, // maxlength
+  "tooShort": false, // minlength
+  "typeMismatch": false, // type
+  "valid": true,
+  "valueMissing": false // required
+}
+```
+
+## New validation flow with error customized
+
+![](./custom-validation-flow.png)
+
+## Validation flow with error synchronized
+
+- When browser validation happens, an invalid event if fired for each invalid input and trigger the error display.
+- We can't check when an invalid input becomes valid again.
+- To get around this we reset the error state before synchronizing the errors.
+
+![](./invalid-event.png)
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,14 +8,78 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+function formatError({
+  input,
+  formData,
+}: {
+  input: HTMLInputElement;
+  formData: FormData;
+}) {
+  switch (input.name) {
+    case "email":
+      if (input.validity.valueMissing) {
+        return "Email is required";
+      } else if (input.validity.typeMismatch) {
+        return "Email is invalid";
+      }
+      break;
+    case "password":
+      if (input.validity.valueMissing) {
+        return "Password is required";
+      } else if (input.validity.tooShort) {
+        return `Password must be at least ${input.minLength} characters`;
+      } else if (input.validity.patternMismatch) {
+        return "Password must contain at least one uppercase and one lowercase letter";
+      }
+      break;
+    case "confirmPassword":
+      if (input.validity.valueMissing) {
+        return "Confirm password is required";
+      } else if (input.value !== formData.get("password")) {
+        return "Passwords do not match";
+      }
+      break;
+  }
+
+  return "";
+}
+
+type FormErrors = {
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+};
+
 export default function SignupForm() {
+  const [error, setError] = useState<FormErrors>({});
   return (
     <form
       method="post"
+      // Called for every invalid input
+      onInvalid={(event) => {
+        const input = event.target as HTMLFormElement;
+        setError((error) => ({
+          ...error,
+          [input.name]: input.validationMessage,
+        }));
+        // Cancel the error display
+        event.preventDefault();
+      }}
       // Disable browser's default validation
       noValidate
       onSubmit={(event) => {
         const form = event.currentTarget;
+        const formData = new FormData(form);
+
+        // Set custom error messages
+        for (const input of form.elements) {
+          if (input instanceof HTMLInputElement) {
+            input.setCustomValidity(formatError({ input, formData }));
+          }
+        }
+
+        setError({});
+
         // Prevent form submission if constraint
         // validation fails
         if (!form.reportValidity()) {
@@ -24,7 +89,14 @@ export default function SignupForm() {
     >
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" required />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className={error.email ? "error" : ""}
+        />
+        <p>{error.email}</p>
       </div>
       <div>
         <label htmlFor="passwd">Password</label>
@@ -35,11 +107,20 @@ export default function SignupForm() {
           required
           minLength={8}
           pattern="(?=.*?[a-z])(?=.*?[A-Z]).*"
+          className={error.password ? "error" : ""}
         />
+        <p>{error.password}</p>
       </div>
       <div>
         <label htmlFor="c-passwd">Confirm Password</label>
-        <input id="c-passwd" name="confirmPassword" type="password" required />
+        <input
+          id="c-passwd"
+          name="confirmPassword"
+          type="password"
+          required
+          className={error.confirmPassword ? "error" : ""}
+        />
+        <p>{error.confirmPassword}</p>
       </div>
       <button>Signup</button>
     </form>
